d7fd9e46f4e1407e2cb9e5dc79af0a90dd9619cb
Danut Ene <danut.ene@sky.uk>
2023-06-02 14:44:32 +0100

    added comments for documentation purposes

--- UniqueChars/main.go
+++ UniqueChars/main.go
@@ -9,14 +9,20 @@ func main() {
 }
 
 func UniqueChars(str string) bool {
+	// Create a map to store characters as keys and their existence as values
 	chars := make(map[rune]bool)
 
+	// Iterate over each character in the string
 	for _, char := range str {
+		// Check if the character already exists in the chars map
 		if chars[char] {
+			// If the character already exists, it's a duplicate, so return false
 			return false
 		}
+		// Mark the character as seen by setting its value to true in the charSet map
 		chars[char] = true
 	}
 
+	// If we reach this point, it means there are no duplicate characters in the string
 	return true
 }
